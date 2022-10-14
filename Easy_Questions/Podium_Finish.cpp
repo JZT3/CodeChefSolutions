691d26d07393222121b476198ed15893443c6987
Jervani Z Thompson <jzthompson20@gmail.com>
2022-10-13 22:55:17 -0500

    Create Podium_Finish.cpp

---
+++ Easy_Questions/Podium_Finish.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	short t,a,b,gap;
+	
+	cin>>t;
+	
+	while(t--){
+	    cin>>a>>b;
+	    gap=a+b;
+	    
+	    cout<<gap<<endl;
+	}
+	return 0;
+}
